70c3af2a56f9f36f7927c26e9f93bf43eb866987
Lucas Cordeiro Araujo <lucascordeiroaraujo@gmail.com>
2021-01-23 02:22:32 -0300

    :recycle: remove random sort in itineraries list

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Header from '~/components/header'
+import Header from '~/components/header';
 
 import Travels from './components/travels';
 
@@ -14,7 +14,13 @@ import Newsletter from '~/components/newsletter';
 
 import Footer from '~/components/footer';
 
-import { citiesAndMonths, itineraries, youtubeVideos, testimonials, contactPage } from '~/interfaces';
+import {
+  citiesAndMonths,
+  itineraries,
+  youtubeVideos,
+  testimonials,
+  contactPage,
+} from '~/interfaces';
 
 interface Iprops {
   homeData: {
@@ -23,32 +29,29 @@ interface Iprops {
     videos: youtubeVideos[];
     testimonials: testimonials;
     contact: contactPage;
-  }
-};
+  };
+}
 
 const pageHome: React.FC<Iprops> = ({ homeData }) => (
   <>
     <Header contact={homeData.contact} />
 
     {homeData.itineraries && (
-      <Travels 
+      <Travels
         citiesAndMonths={homeData.citiesAndMonths}
-        itineraries={homeData.itineraries.sort(() => Math.random() - 0.5)} 
+        itineraries={homeData.itineraries}
       />
     )}
 
     {homeData.testimonials && (
-      <Moments 
-        testimonials={homeData.testimonials.acf.testimonials} 
+      <Moments
+        testimonials={homeData.testimonials.acf.testimonials}
         hasVideos={homeData.videos ? true : false}
       />
     )}
 
     {homeData.videos && (
-      <Videos 
-        videos={homeData.videos} 
-        contact={homeData.contact}
-      />
+      <Videos videos={homeData.videos} contact={homeData.contact} />
     )}
 
     <Pictures />
